5a982429ecf94166bee3cbda2fdb61d49d1dfdc1
tigran-kocharyan <tigran.kocharyan@outlook.com>
2019-02-02 18:17:17 +0500

    Update cats.py

--- cats.py
+++ cats.py
@@ -50,7 +50,7 @@ def get_callback_from_button(bot, update):
 
 def sendgif(bot, update):
     bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.UPLOAD_DOCUMENT)
-    bot.sendAnimation(chat_id=update.message.chat_id, animation='https://media.giphy.com/media/Z55zlzompYguc/giphy.gif'
+    bot.sendAnimation(chat_id=update.message.chat_id, animation='https://media.giphy.com/media/Z55zlzompYguc/giphy.gif')
 
 def main():
     updater = Updater("721020886:AAEFdaOC6LZcM9T2x-D5QpBl6MEvAwLZ8LU")
